0b9596104627fd03a334821b1125cf362bfc377a
Gaétan Dubuc <gaetan@MacBook-Air-de-Gaetan.local>
2025-11-15 15:34:05 +0100

    Implement a REPL

--- app/main.py
+++ app/main.py
@@ -1,12 +1,30 @@
 import sys
 
 
+def read() -> str:
+    sys.stdout.write("$ ")
+    return input()
+
+
+known_cmds = {}
+
+
+def eval_command(cmd: str) -> str:
+    if cmd in known_cmds:
+        return ""
+    return f"{cmd}: command not found"
+
+
+def print_result(result: str) -> None:
+    sys.stdout.write(result + "\n")
+
+
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    cmd = input()
-    sys.stdout.write(f"{cmd}: command not found")
-    pass
+    while True:
+        cmd = read()
+        result = eval_command(cmd)
+        print_result(result)
 
 
 if __name__ == "__main__":
